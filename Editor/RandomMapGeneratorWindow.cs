248737d6619ae3fbd733e8fbfb2786bc61e931b5
Richard Guilliams <dropletswebdevelopment@gmail.com>
2024-04-25 22:21:40 -0600

    New WFCGenerator Class Added - Working

--- Editor/RandomMapGeneratorWindow.cs
+++ Editor/RandomMapGeneratorWindow.cs
@@ -67,129 +67,6 @@ public class RandomMapGeneratorWindow : EditorWindow
         DestroyImmediate(tilemapObject.GetComponent<Rigidbody2D>());
         DestroyImmediate(tilemapObject.GetComponent<CompositeCollider2D>());
     }
-
-    // public void AssignEntropyMap(){
-    //     map = new int[maxWidth, maxLength, maxHeight];
-    //     Random.InitState(seed);
-    //     for(int i = 0; i < maxWidth; i++){
-    //         for(int j = 0; j < maxLength; j++){
-    //             for(int k = 0; k < maxHeight; k++){
-    //                 // Locking the generation to the lowest z axis
-    //                 if(k == 0){
-    //                     map[i, j, k] = tiles.tileGenerationList.Count;
-    //                 }
-    //             }   
-    //         }       
-    //     }
-    // }
-
-    // public void ChooseRandomPosition(){
-    //     selectedPosition.x = Random.Range(0, maxHeight - 1);
-    //     selectedPosition.y = Random.Range(0, maxWidth - 1);
-    // }
-
-    // public TileGenerationData GetDataByWeight(){
-    //     int weightSum = 0;
-    //     foreach(TileGenerationData tile in tiles.tileGenerationList) {
-    //         weightSum += tile.weight;
-    //     }
-    //     int currentWeight = Random.Range(0, weightSum);
-    //     for(int i = 0;  i < tiles.tileGenerationList.Count; i++){
-    //         currentWeight -= tiles.tileGenerationList[i].weight;
-    //         if(currentWeight <= 0) return tiles.tileGenerationList[i];
-    //     }
-    //     return new TileGenerationData();
-    // }
-
-    // public void GetRandomTile(){
-        
-    // }
-
-    // public void GetEntropyData(){
-        
-    // }
-
-    // public Vector3Int GetRandomMapPosition(){
-    //     int x = Random.Range(0, maxWidth);
-    //     int y = Random.Range(0, maxLength);
-    //     int z = Random.Range(0, 0);
-    //     return new Vector3Int(x, y, z);
-    // }
-    // public void CollapseFirstCell(){
-    //     Vector3Int position = GetRandomMapPosition();
-    //     currentTile = GetDataByWeight();
-    //     tilemap.SetTile(position, currentTile.tileData.tileBase);
-    //     map[position.x, position.y, position.z] = 0;
-    //     RecalculateEntropyData();
-    // }
-
-    // public void AssignEntropyAtPosition(Vector3Int position, int entropy){
-    //     map[position.x, position.y, position.z] = entropy;
-    // }
-    // public TileGenerationList RecalculateEntropyData(){
-    //     // from selected position, readjust the entropy of the surrounding cells that have not been collapsed.;
-    //     Vector3Int left = new Vector3Int(selectedPosition.x - 1, selectedPosition.y, 0);
-    //     Vector3Int right = new Vector3Int(selectedPosition.x + 1, selectedPosition.y, 0);
-    //     Vector3Int up = new Vector3Int(selectedPosition.x, selectedPosition.y - 1, 0);
-    //     Vector3Int down = new Vector3Int(selectedPosition.x, selectedPosition.y + 1, 0);
-    //     if(left.x > 0){
-    //         AssignEntropyAtPosition(left, currentTile.possibleTiles.Count);
-    //     }
-    //     if(right.x < maxWidth){
-    //         AssignEntropyAtPosition(right, currentTile.possibleTiles.Count);
-    //     }
-    //     if(down.y > 0){
-    //         AssignEntropyAtPosition(up, currentTile.possibleTiles.Count);
-    //     }
-    //     if(up.y < maxHeight){
-    //         AssignEntropyAtPosition(down, currentTile.possibleTiles.Count);
-    //     }
-    //     TileGenerationList newGenerationList = new TileGenerationList();
-    //     foreach(EntropyData data in currentTile.possibleTiles){
-    //         newGenerationList.tileGenerationList.Add(data.tile);
-    //     }
-    //     return newGenerationList;
-    // }
-
-    // public List<Vector3Int> GetPositionsWithLowestEntropy(){
-    //     List<Vector3Int> newList = new List<Vector3Int>();
-    //     int currentLowest = tiles.tileGenerationList.Count;
-    //     for(int i = 0; i < maxWidth; i++){
-    //         for(int j = 0; j < maxLength; j++){
-    //             for(int k = 0; k < maxHeight; k++){
-    //                 if(currentLowest > map[i, j, k] && map[i, j, k] != 0) currentLowest = map[i, j, k];
-    //             }
-    //         }
-    //     }
-    //     for(int i = 0; i < maxWidth; i++){
-    //         for(int j = 0; j < maxLength; j++){
-    //             for(int k = 0; k < maxHeight; k++){
-    //                 if(map[i, j, k] == currentLowest) newList.Add(new Vector3Int(i, j, k));
-    //             }
-    //         }
-    //     }
-    //     return newList;
-    // }
-
-    // public void SelectNextTilePosition(){
-    //     List<Vector3Int> LowestEntropyPositions = GetPositionsWithLowestEntropy();
-    //     selectedPosition = LowestEntropyPositions[Random.Range(0, LowestEntropyPositions.Count - 1)];
-    //     // from selectionPosition we need to get the tileGenerationDataAssociated with the tile in the selected position.
-    //     // We need generate a new tileGenerationList from the tiles in the entropy data
-    //     // 
-    // }
-    // public void CollapseCells(){
-    //     for(int i = 0; i < maxWidth; i++){
-    //         for(int j = 0; j < maxLength; j++){
-    //             for(int k = 0; k < maxHeight; k++){
-    //                 SelectNextTilePosition();
-    //             }
-    //         }
-    //     }
-    //     //assign one of the tiles from the list to the currentTile.
-    //     GetRandomTile();
-        // currentTile = tiles.tileGenerationList[3];
-    // }
 }
 
 public class WFCGenerator{
@@ -200,6 +77,8 @@ public class WFCGenerator{
     public int mapWidth;
     public int mapLength;
     public int mapHeight;
+
+    public bool loop = true;
 
     public void InitRandomSeed(int seed){
         Random.InitState(seed);
@@ -214,8 +93,8 @@ public class WFCGenerator{
 
     public void InitializeCells(){
         for(int i = 0; i < mapHeight - 1; i++){
-            for(int j = 0; j < mapWidth - 1; j++){
-                for(int k = 0; k < mapLength - 1; k++){
+            for(int j = 0; j < mapLength - 1; j++){
+                for(int k = 0; k < mapWidth - 1; k++){
                     cells.Add(new Cell(new Vector3Int(j, k, i), tiles));
                 }
             }
@@ -227,15 +106,15 @@ public class WFCGenerator{
         foreach(var cell in cells){
             cell.leftCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x - 1, cell.cellPosition.y, cell.cellPosition.z));
             cell.rightCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x + 1, cell.cellPosition.y, cell.cellPosition.z));
-            cell.upCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x, cell.cellPosition.y - 1, cell.cellPosition.z));
-            cell.downCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x, cell.cellPosition.y + 1, cell.cellPosition.z));
-            cell.aboveCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x, cell.cellPosition.y, cell.cellPosition.z + 1));
+            cell.upCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x, cell.cellPosition.y + 1, cell.cellPosition.z));
+            cell.downCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x, cell.cellPosition.y - 1, cell.cellPosition.z));
+            cell.aboveCell = cells.Find(c => c.cellPosition == new Vector3Int(cell.cellPosition.x, cell.cellPosition.y, cell.cellPosition.z - 1));
         }
     }
 
 
     public List<Cell> CellsWithLowestEntropy(){
-        int minEntropy = cells.Count;
+        int minEntropy = tiles.tileGenerationList.Count;
         foreach(Cell cell in cells){
             if(cell.entropy > 0 && cell.entropy < minEntropy) minEntropy = cell.entropy;
         }
@@ -245,46 +124,55 @@ public class WFCGenerator{
     
     //Pick random cell from list of cells with lowest entropy as long as entropy is not 0;
     public void GetRandomCellWithLowestEntropy(){
-        currentCell = CellsWithLowestEntropy()[Random.Range(0, CellsWithLowestEntropy().Count)];
+        if(CellsWithLowestEntropy().Count > 0){
+            currentCell = CellsWithLowestEntropy()[Random.Range(0, CellsWithLowestEntropy().Count)];
+        }
     }
     // pick random tile from current tiles possibleTiles.
-    public TileGenerationData GetRandomTile(){
-        return currentCell.possibleTiles.tileGenerationList[Random.Range(0, currentCell.possibleTiles.tileGenerationList.Count - 1)];
+    public EntropyData GetRandomTile(){
+        int weightSum = 0;
+        foreach(EntropyData data in currentCell.possibleTiles){
+            weightSum += data.weight;
+        }
+        int currentWeight = Random.Range(0, weightSum);
+        for(int i = 0; i < currentCell.possibleTiles.Count; i++){
+            currentWeight -= currentCell.possibleTiles[i].weight;
+            if(currentWeight <= 0) return currentCell.possibleTiles[i];
+        }
+        return currentCell.possibleTiles[Random.Range(0, currentCell.possibleTiles.Count - 1)];
     }
     //Set the random tile from the map
     public void SetTile(){
-        currentCell.tile = GetRandomTile();
+        currentCell.tile = GetRandomTile().tile;
         tilemap.SetTile(currentCell.cellPosition, currentCell.tile.tileData.tileBase);
     }
     // recalculate the neighboring cells entropy.
     public void RecalculateEntropyData(){
-        List<TileGenerationData> neighboringTiles = new List<TileGenerationData>();
-        // find all neighboring cells that are not assigned
+        //set current cells entropy to 0;
+        currentCell.entropy = 0;
+
+        //get list of neighboring cells.
+        // for each neighboring cell 
+        List<Cell> neighboringCells = currentCell.NeighboringCells();
         foreach(Cell cell in currentCell.NeighboringCells()){
-            // find the tileBase for this cell
-                if(!cell.assigned){
-                cell.tile = GetRandomTile();
-                TileBase tile = cell.tile.tileData.tileBase;
-                neighboringTiles.Add(tiles.tileGenerationList.Find(t => t.tileData.tileBase == tile));
+            // - get the possible tiles list for that tiles neighbors.
+            List<List<EntropyData>> possibleTiles = new List<List<EntropyData>>();
+            List<EntropyData> commonTiles = new List<EntropyData>();
+            foreach(Cell neighborCell in cell.NeighboringCells()){
+                // add the possibletiles of the neighborCell to the possibleTiles
+                possibleTiles.Add(neighborCell.possibleTiles);
+                // - get the common tiles from each list of possible tiles.
             }
-        };
-        // get the lists of the possible tiles from all TileGenerationData in neighboring cells for each tile.
-        List<List<EntropyData>> possibleTilesList = new List<List<EntropyData>>();
-        foreach(TileGenerationData tile in neighboringTiles){
-            possibleTilesList.Add(tile.possibleTiles);
+            // get list of all possible tiles that exist in all lists
+            commonTiles = possibleTiles[0];
+            for(int i = 1; i < possibleTiles.Count - 1; i++){
+            commonTiles = commonTiles.Intersect(possibleTiles[i]).ToList();
+            }
+            // - assign the common tiles to the possiible tiles list of cell in loop.
+            cell.possibleTiles = commonTiles;
+            cell.entropy = cell.possibleTiles.Count;
+            // - assign the entropy data of the cell in loop to its possibleTiles.Count.
         }
-        // get list of all possible tiles that exist in all lists
-        List<EntropyData> commonTiles = possibleTilesList[0];
-        for(int i = 1; i < possibleTilesList.Count - 1; i++){
-            commonTiles = commonTiles.Intersect(possibleTilesList[i]).ToList();
-        }
-        // get a list of all generationData from the common tiles and set in to the possibleTiles list for the currentCell;
-        TileGenerationList newGenerationList = new TileGenerationList();
-        newGenerationList.tileGenerationList = new List<TileGenerationData>();
-        foreach(EntropyData entropyData in commonTiles){
-            newGenerationList.tileGenerationList.Add(entropyData.tile);
-        }
-        currentCell.possibleTiles = newGenerationList;
     }
 
     public bool AllTilesAssigned(){
@@ -303,11 +191,11 @@ public class WFCGenerator{
         List<Cell> lowestHeightCells = cells.Where(cell => cell.cellPosition.z == 0).ToList();
         // get a random cell from the new List
         currentCell = lowestHeightCells[Random.Range(0, lowestHeightCells.Count - 1)];
-        currentCell.tile = GetRandomTile();
+        currentCell.tile = GetRandomTile().tile;
         tilemap.SetTile(currentCell.cellPosition, currentCell.tile.tileData.tileBase);
-        RecalculateEntropyData();
         currentCell.entropy = 0;
         currentCell.assigned = true;
+        RecalculateEntropyData();
 
     }
     // repeat cycle until all cells have an entropy of 0;
@@ -315,10 +203,11 @@ public class WFCGenerator{
         //
         collapseFirstCell();
         // iterate through the list until all tiles have been assigned
-        while(!AllTilesAssigned()){
+        while(!AllTilesAssigned() && loop){
             GetRandomCellWithLowestEntropy();
             SetTile();
             RecalculateEntropyData();
+            currentCell.entropy = 0;
             currentCell.assigned = true;
         }
         
@@ -337,7 +226,7 @@ public class Cell{
     public TileGenerationData tile = new TileGenerationData();
     public Vector3Int cellPosition;
     public int entropy;
-    public TileGenerationList possibleTiles;
+    public List<EntropyData> possibleTiles;
     public List<Cell> neighbors = new List<Cell>();
     public Cell leftCell;
     public Cell rightCell;
@@ -350,8 +239,9 @@ public class Cell{
 
     public Cell(Vector3Int position, TileGenerationList tileList){
         cellPosition = position;
-        possibleTiles = tileList;
-        entropy = possibleTiles.tileGenerationList.Count;
+        possibleTiles = tileList.possibleTiles;
+        tile = tileList.tileGenerationList[0];
+        entropy = possibleTiles.Count;
     }
 
     public List<Cell> NeighboringCells(){
--- WFC Map Generator/TileGenerationList.cs
+++ WFC Map Generator/TileGenerationList.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Tile Generation List", menuName = "Tile Generation List")]
 public class TileGenerationList : ScriptableObject{
+    public List<EntropyData> possibleTiles = new List<EntropyData>();
     public List<TileGenerationData> tileGenerationList;
 }
